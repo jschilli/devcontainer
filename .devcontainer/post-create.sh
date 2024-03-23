84e7f9b3c9a16679b863168ae85d0f080ea2598d
Jeff Schilling <jeff.schilling@gmail.com>
2024-03-23 15:37:45 +0000

    Cleanup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -18,4 +18,3 @@ chown vscode:vscode ~vscode/.cargo
 # and https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
 git config --global --add safe.directory $1
 
-# /usr/local/cargo/bin/cargo install cargo-component
